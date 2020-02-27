e370de1b984b3fe5eb51f70fe98bca02c3257b92
Lillie Robinson <lerobinson98@yahoo.com>
2020-02-27 15:24:51 -0700

    Testing out how this works
    Signed-off-by: Lillie Robinson <lerobinson98@yahoo.com>

---
+++ CSE360/src/assign1/SimpleList.java
@@ -0,0 +1,109 @@
+package assign1;
+
+
+public class SimpleList {
+	
+	private int[] list;
+	private int count;
+	
+	public SimpleList() {
+		count = 0;
+		int num = 10;
+		list = new int[num];
+	}
+	
+	public void Add(int to_be_added) {
+		
+		count = 0;
+		
+		for (int i = 0; i < list.length; i++) {
+			if (list[i] == 0) {
+				list[i] = to_be_added;
+				count++;
+				break;
+			}
+		}		
+	}
+
+
+	public void Remove(int to_be_removed) {
+		
+		//create a reference variable for help with moving
+		int index = 0;
+		//iterate through list 
+		for(int i = 0; i < list.length; i++) {
+			
+			//move the element to the next space when not removing 
+			if(list[i] != to_be_removed) {
+				list[index++] = list[i];
+			}
+		}
+		int num = (int) (10*.75);
+		System.out.println(num);
+		
+	}
+	
+	public int count() {
+		count = 0;
+		//count the number of items in array list
+		for(int k = 0; k < list.length; k++) {
+			
+			if(list[k] != 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public String toString() {
+		//returns the array list as a String with a space between each variable and no space after the last element
+		
+		
+		
+		//declare an empty string 
+		String s = "";
+		
+		//turn each element into a string with a space after
+		for(int i = 0; i < list.length; i++) {
+			
+			if(list[i] != 0) {
+			
+			s = s + Integer.toString(list[i]) + " ";
+			}
+						
+		}
+		
+		//get the substring that excludes the last space in the program 
+		String str = s.substring(0, s.length() - 1);
+		
+		return str; 
+		
+		
+	}
+	
+	public int search(int to_be_searched_for) {
+		//search the list for the variable and return the location. If not present, return -1
+		
+		
+		//create external iterator for for loop
+		int k = 0;
+		
+		int n = list.length;
+
+		//loop through each element checking if value is present
+		for(k = 0; k < n; k++) {
+			
+			if (list[k] == to_be_searched_for) { 
+				return k;		
+			}
+			
+		}
+		
+			return -1;
+		}
+}
+		
+	
+	
+
+
---
+++ CSE360/src/assign1/SimpleListTest.java
@@ -0,0 +1,131 @@
+package assign1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleListTest {
+
+	@Test
+	void test_add() {
+		//fail("Not yet implemented");
+		
+		//create an object of type SimpleList
+		SimpleList sl = new SimpleList();
+		
+		//add the number 4 to the array 
+		sl.Add(4);
+		
+		//add the number 8 to the array
+		sl.Add(8);
+		
+		//add the number 2 to the array
+		sl.Add(2);
+
+		//create an integer to store the observed value recorded in add()
+		int add_test = sl.count();
+		
+		//test if the expected value is the same as the add_test value
+		assertEquals(3, add_test); 
+		
+	}
+	
+	@Test
+	void test_Remove() {
+		//fail("Not yet implemented");
+		
+		SimpleList sl = new SimpleList();
+		
+		//add the number 4 to the array
+		sl.Add(4);
+		
+		//add the number 8 to the array
+		sl.Add(8);
+		
+		//add the number 2 to the array
+		sl.Add(2);
+		
+		//remove one to test if the remove method works
+		sl.Remove(8);
+		
+		//create an integer to store the observed value recorded in remove()
+		int remove_test = sl.count();
+		
+		//test if the expected value is the same as observed
+		assertEquals(2, remove_test);
+	}
+	
+	
+	@Test
+	void test_toString() {
+		//fail("Not yet implemented");
+		
+		SimpleList sl = new SimpleList();
+		
+		//add the number 4 to the array 
+		sl.Add(4);
+				
+		//add the number 8 to the array
+		sl.Add(8);
+				
+		//add the number 2 to the array
+		sl.Add(2);
+		
+		//store the observed value of the toString() method
+		String str = sl.toString();
+		
+		//test if the expected value is the same as observed for toString()
+		assertEquals("4 8 2", str);
+	}
+	
+	@Test
+	void test_search() {
+		//fail("Not yet implemented");
+		
+		SimpleList sl = new SimpleList();
+		
+		//add the number 4 to the array 
+		sl.Add(4);
+						
+		//add the number 8 to the array
+		sl.Add(8);
+							
+		//add the number 2 to the array
+		sl.Add(2);
+		
+		//create an integer to store the observed value of search test
+		int search_test = sl.search(2);
+		
+		//test if the expected and observed match for search()
+		assertEquals(2, search_test);
+		
+		
+		
+	}
+	
+	@Test
+	void test_count() {
+		
+		SimpleList sl = new SimpleList();
+		
+		//add the number 4 to the array 
+		sl.Add(4);
+				
+		//add the number 8 to the array
+		sl.Add(8);
+						
+		//add the number 2 to the array
+		sl.Add(2);
+		
+		
+		int count_test = sl.count();
+		
+		assertEquals(3, count_test);
+
+	}
+	
+	
+	
+	
+
+}
